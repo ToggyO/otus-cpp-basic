79a2d5074ef156a2f9abe35b69c6c789c176dd3e
otogushakov <otogushakov@ptsecurity.com>
2023-02-13 10:35:50 +0700

    HW4 - fix review comments

--- include/tree/add.hpp
+++ include/tree/add.hpp
@@ -6,5 +6,4 @@ class Add : public ASTNode
 {
     public:
         Add(ASTNode *left_hand, ASTNode *right_hand);
-        ~Add();
 };
--- src/nodes/add.cpp
+++ src/nodes/add.cpp
@@ -4,8 +4,3 @@ const static char *add_operator = "+";
 
 Add::Add(ASTNode *left_hand, ASTNode *right_hand)
     : ASTNode(add_operator, left_hand, right_hand) {}
-
-Add::~Add()
-{
-    std::cout << "~Add()" << std::endl;
-}
